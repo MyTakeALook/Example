5874783167db991b6c1291c020760a60984f1d03
Angelachaejung <a01083298599@gmail.com>
2023-01-19 01:23:34 +0900

    디테일페이지 css

--- src/pages/Comments/AddComment.jsx
+++ src/pages/Comments/AddComment.jsx
@@ -40,19 +40,21 @@ const AddCommentForm = () => {
           maxLength={5}
         /> */}
       </StNameInput>
-      <input
-        placeholder="댓글을 추가하세요. (100자 이내)"
-        name="content"
-        type="text"
-        onChange={(ev) => {
-          setComment({
-            ...comment,
-            comment: ev.target.value,
-          });
-        }}
-        maxLength={100}
-      />
-      <button type="submit">추가하기</button>
+      <StAddinput>
+        <StInput
+          placeholder="댓글을 추가하세요. (100자 이내)"
+          name="content"
+          type="text"
+          onChange={(ev) => {
+            setComment({
+              ...comment,
+              comment: ev.target.value,
+            });
+          }}
+          maxLength={100}
+        />
+        <StButton type="submit">추가하기</StButton>
+      </StAddinput>
     </StForm>
   );
 };
@@ -67,4 +69,33 @@ const StForm = styled.form`
   gap: 12px;
   width: 100%;
   padding: 0 12px;
+`;
+const StAddinput = styled.div`
+  margin: 40px auto auto 30px;
+`;
+const StInput = styled.input`
+  width: 400px;
+  height: 40px;
+  border-radius: 20px;
+  border: solid white 1px;
+  background-color: #f1f1f1;
+  padding-left: 10px;
+`;
+const StButton = styled.button`
+  margin: -40px auto auto 420px;
+  background-color: black;
+  text-align: center;
+  width: 70px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50px;
+  border: 1px solid black;
+  font-weight: bold;
+  font-size: 13px;
+  color: white;
+  cursor: pointer;
+  /* font-family: "Noto Sans KR", sans-serif; */
 `;
